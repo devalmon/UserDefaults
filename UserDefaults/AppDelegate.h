b03f4dc0d6f324771c85836ad4b36ba3209e2319
devalmon <phlood@yandex.ru>
2020-06-08 14:54:28 +0300

    Initial Commit

---
+++ UserDefaults/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UserDefaults
+//
+//  Created by Alexey Baryshnikov on 08.06.2020.
+//  Copyright © 2020 Alexey Baryshnikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ UserDefaults/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  UserDefaults
+//
+//  Created by Alexey Baryshnikov on 08.06.2020.
+//  Copyright © 2020 Alexey Baryshnikov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
